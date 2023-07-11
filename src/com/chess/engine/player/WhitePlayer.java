4e773c8ca694361a6e20076b5379957fefc435c1
ABHINAVGUPTA02 <abhinav.gupta.ag47@gmail.com>
2023-07-12 04:14:53 +0530

    Errors resolved and started creating the GUI

--- src/com/chess/engine/player/WhitePlayer.java
+++ src/com/chess/engine/player/WhitePlayer.java
@@ -4,6 +4,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
+import com.chess.engine.pieces.King;
 import com.chess.engine.pieces.Piece;
 import com.chess.engine.pieces.Rook;
 
