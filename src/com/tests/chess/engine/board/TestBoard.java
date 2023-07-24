073d5dfd51c506ae50c37ac240f484b60da5aef6
ABHINAVGUPTA02 <abhinav.gupta.ag47@gmail.com>
2023-07-24 19:16:47 +0530

    Unit Test added

---
+++ src/com/tests/chess/engine/board/TestBoard.java
@@ -0,0 +1,24 @@
+package com.tests.chess.engine.board;
+
+import com.chess.engine.board.Board;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestBoard {
+    @Test
+    public void initialBoard(){
+        final Board board = Board.createStandardBoard();
+        assertEquals(board.getCurrentPlayer().getLegalMoves().size(),20);
+        assertEquals(board.getCurrentPlayer().getOpponent().getLegalMoves().size(),20);
+        assertFalse(board.getCurrentPlayer().isInCheck());
+        assertFalse(board.getCurrentPlayer().isInCheckMate());
+        assertFalse(board.getCurrentPlayer().isCastled());
+
+        assertEquals(board.getCurrentPlayer(), board.whitePlayer());
+        assertEquals(board.getCurrentPlayer().getOpponent(), board.blackPlayer());
+        assertFalse(board.getCurrentPlayer().getOpponent().isInCheck());
+        assertFalse(board.getCurrentPlayer().getOpponent().isInCheckMate());
+        assertFalse(board.getCurrentPlayer().getOpponent().isCastled());
+    }
+}
