f6c19cc1d820116821aa42bc969f489871ce455f
ABHINAVGUPTA02 <abhinav.gupta.ag47@gmail.com>
2023-07-24 19:28:41 +0530

    AI package created

--- src/com/tests/chess/engine/board/TestBoard.java
+++ src/com/tests/chess/engine/board/TestBoard.java
@@ -9,6 +9,7 @@ public class TestBoard {
     @Test
     public void initialBoard(){
         final Board board = Board.createStandardBoard();
+
         assertEquals(board.getCurrentPlayer().getLegalMoves().size(),20);
         assertEquals(board.getCurrentPlayer().getOpponent().getLegalMoves().size(),20);
         assertFalse(board.getCurrentPlayer().isInCheck());
